2542e36a8379141c6f2af7f02ac71f4646e50386
DiegoQuintero05 <diego.quintero@beecker.ai>
2023-09-14 09:21:54 -0600

    Fix Upload
    Ya sube archivos a drive, hay que estructurar codigo, y adaptarlo

--- BeeckerLab_Drive/BeeckerLab_Drive/Class1.cs
+++ BeeckerLab_Drive/BeeckerLab_Drive/Class1.cs
@@ -3,6 +3,7 @@ using Google.Apis.Drive.v3;
 using Google.Apis.Services;
 using System;
 using System.IO;
+using System.Threading;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,48 +15,71 @@ namespace BeeckerLab_Drive
     {
 
 
-        static void Main()
+        static async Task Main(string[] args)
         {
-            string path = "1MdJrBaBT9x8792VEh4fXipXItrTgWJTn";
+            string path = "1nPFSsAY4thQfJnmzzos6zG0pp7pCC2Td";
             string credentialsPath = "credentials.json";
-            string[] FilePaths = { @"D:\BeeckerTrabajo\BeeckerLab_Drive\BeeckerLab_Drive\bin\Debug\image.jpg", @"D:\BeeckerTrabajo\BeeckerLab_Drive\BeeckerLab_Drive\bin\Debug\image2.png" };
-            Console.WriteLine(FilePaths[1]);
-            CargadeArchivos(path,credentialsPath,FilePaths);
+            string FilePaths =  "image.jpg" ;
+            //CargadeArchivos(path,credentialsPath,FilePaths);
+            // Directory.GetFile - Obtener Path 
+
+            var credential = GoogleCredential.FromFile(credentialsPath)
+                .CreateScoped(DriveService.ScopeConstants.Drive);
+
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential
+            });
+
+            var fileMetadata = new Google.Apis.Drive.v3.Data.File()
+            {
+                Name = "Test Upload1.jpg",
+                Parents = new List<string>() { "1nPFSsAY4thQfJnmzzos6zG0pp7pCC2Td" }
+            };
+
+            string uploadedFileId;
+
+              using (var fsSource = new FileStream(FilePaths,FileMode.Open,FileAccess.Read))
+            {
+                var request = service.Files.Create(fileMetadata, fsSource, "");
+                request.Fields = "*";
+                var results =  await request.UploadAsync(CancellationToken.None);
+
+                if(results.Status == Google.Apis.Upload.UploadStatus.Failed)
+                {
+                    Console.WriteLine($"Error uploading file: {results.Exception.Message}");
+                }
+                uploadedFileId = request.ResponseBody?.Id;
+            }
+
+            var uploadedFile = await service.Files.Get(uploadedFileId).ExecuteAsync();
+            Console.WriteLine($"{uploadedFile.Id} {uploadedFile.Name}{uploadedFile.MimeType} {uploadedFile.Parents?.FirstOrDefault()}");
+
+
+
+
         }
+
+
 
         public static Google.Apis.Drive.v3.Data.File CargadeArchivos(string folderID, string credentialsPath, string[] FilePaths)
         {
             try
             {
                 // Get Credentials
-                GoogleCredential credential;
-
-                using (var stream = new FileStream(credentialsPath, FileMode.Open, FileAccess.Read))
-                {
-                    credential = GoogleCredential.FromStream(stream).CreateScoped(new[]
-                    {
-                        DriveService.ScopeConstants.DriveFile
-                    });
-                }
-
-                
+                GoogleCredential credential = GetCredentials(credentialsPath);
 
                 // Create Drive API service.
-                var service = new DriveService(new BaseClientService.Initializer
-                {
-                    HttpClientInitializer = credential,
-                    ApplicationName = "Google Drive Upload"
-                });
+                var service =  CreateService(credential);
+                
+
 
                 // Upload File
                 foreach (var filePath in FilePaths)
                 {
                     // Get File Data
-                    var fileMetadata = new Google.Apis.Drive.v3.Data.File()
-                    {
-                        Name = Path.GetFileName(filePath),
-                        Parents = new List<string> { folderID }
-                    };
+                    var fileMetadata = GetFileData(filePath, folderID);
+                    
                     FilesResource.CreateMediaUpload request;
                     // Create a new file on drive.
                     using (var stream = new FileStream(filePath,
@@ -117,6 +141,26 @@ namespace BeeckerLab_Drive
                     });
             }
             return credential;
+        }
+
+        public static DriveService CreateService(GoogleCredential credential)
+        {
+            var service = new DriveService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "Google Drive Upload"
+            });
+            return service;
+        }
+
+        public static Google.Apis.Drive.v3.Data.File GetFileData(string filePath, string folderID)
+        {
+            var fileMetadata = new Google.Apis.Drive.v3.Data.File()
+            {
+                Name = Path.GetFileName(filePath),
+                Parents = new List<string> { folderID }
+            };
+            return fileMetadata;
         }
 
     }
